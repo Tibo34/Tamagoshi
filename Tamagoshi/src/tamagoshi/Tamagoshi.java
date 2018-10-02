5dc21337b8c692d69ef325f0b90de55884c0b767
Thibaut Molina <thibautmolina@yahoo.fr>
2018-10-02 12:54:42 +0200

    Création classe Tamagoshi

---
+++ Tamagoshi/src/tamagoshi/Tamagoshi.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package tamagoshi;
+
+/**
+ * @author thiba
+ *
+ */
+class Tamagoshi {
+
+	/**
+	 * 
+	 */
+	public Tamagoshi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
